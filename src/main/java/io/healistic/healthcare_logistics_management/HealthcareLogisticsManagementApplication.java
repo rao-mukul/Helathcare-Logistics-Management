85546a41afc3ba2ad34589834136c6b2e31b1acb
rao-mukul <mukultheprogrammer@gmail.com>
2024-05-02 14:00:10 +0530

    Initial Commit

---
+++ src/main/java/io/healistic/healthcare_logistics_management/HealthcareLogisticsManagementApplication.java
@@ -0,0 +1,13 @@
+package io.healistic.healthcare_logistics_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareLogisticsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcareLogisticsManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/healistic/healthcare_logistics_management/HealthcareLogisticsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.healistic.healthcare_logistics_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareLogisticsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
